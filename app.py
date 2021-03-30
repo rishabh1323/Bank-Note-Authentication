e9c5d0d1da4aae126855bb0290a43e2b276e563c
Rishabh <dingliwalr@gmail.com>
2021-03-30 21:55:44 +0530

    Some changes

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import pickle
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for, redirect
 
 app = Flask(__name__)
 classifier = pickle.load(open('model.pkl', 'rb'))[0]
@@ -9,15 +9,21 @@ scaler = pickle.load(open('model.pkl', 'rb'))[1]
 def home():
     return render_template('index.html')
 
-@app.route('/predict')
+@app.route('/predict', methods=['POST'])
 def predict():
-    variance = request.args.get('variance')
-    skewness = request.args.get('skewness')
-    curtosis = request.args.get('curtosis')
-    entropy = request.args.get('entropy')
-    prediction = classifier.predict(scaler.transform([[variance, skewness, curtosis, entropy]]))[0]
-    return "Predicted value is " + str(prediction)
-
+    if(request.method == 'POST'):
+        variance = request.form['variance']
+        skewness = request.form['skewness']
+        curtosis = request.form['curtosis']
+        entropy = request.form['entropy']
+        prediction = classifier.predict(scaler.transform([[variance, skewness, curtosis, entropy]]))[0]
+        
+        if(prediction):
+            prediction_text = 'The Bank Note is a Fake!'
+        else:
+            prediction_text = 'The Bank Note is Authentic!'
+        return render_template('index.html', prediction_text=prediction_text)
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
